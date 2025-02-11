a9f1813c75eacad1ebe2892f6d92b41a0e284eef
saulotarsobc <saulotarsobc@gmail.com>
2025-02-11 17:05:50 -0300

    corrige variáveis de ambiente no script de backup e restauração de banco de dados

--- script.sh
+++ script.sh
@@ -1,19 +1,18 @@
 set -e
 
 # Backup
-if [ "$DO_BACKUP" == "1" ]; then
+if [ "$DO_BACKUP" = "1" ]; then
     echo "Criando backup do banco de dados..."
+    export PGPASSWORD="$SRC_DB_PASS"
     
-    export PGPASSWORD="$PROD_DB_PASS"
-    
-    pg_dump -h "$PROD_DB_HOST" -p "$PROD_DB_PORT" -U "$PROD_DB_USER" -Fc --no-owner --no-privileges -d "$PROD_DB_NAME" -f /files/backup.dump --verbose;
+    pg_dump -h "$SRC_DB_HOST" -p "$SRC_DB_PORT" -U "$SRC_DB_USER" -Fc --no-owner --no-privileges -d "$SRC_DB_NAME" -f /files/backup.dump --verbose;
 fi
 
 # Restore
-if [ "$DO_RESTORE" == "1" ]; then
+if [ "$DO_RESTORE" = "1" ]; then
     echo "Restaurando o banco de dados..."
     
-    export PGPASSWORD="$DEV_DB_PASS"
+    export PGPASSWORD="$DEST_DB_PASS"
     BACKUP_FILE="/files/backup.dump"
     
     # Verifica se o arquivo de backup existe
@@ -23,7 +22,7 @@ if [ "$DO_RESTORE" == "1" ]; then
     fi
     
     # Restaura o banco de dados
-    pg_restore --no-comments --no-owner --disable-triggers --verbose -h "$DEV_DB_HOST" -p "$DEV_DB_PORT" -U "$DEV_DB_USER" -d "$DEV_DB_NAME" /files/backup.dump
+    pg_restore --no-comments --no-owner --disable-triggers --verbose -h "$DEST_DB_HOST" -p "$DEST_DB_PORT" -U "$DEST_DB_USER" -d "$DEST_DB_NAME" /files/backup.dump
     
     # Verifica se a restauração foi bem-sucedida
     if [ $? -eq 0 ]; then
